145df41fadf5dae77b40de6a1935dccc9fbd154d
RenataNorkiene <renata.norkiene@gmail.com>
2021-04-22 12:10:16 +0300

    updated js file

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
 const dataContainer = document.querySelector('#data'); 
 
-
-const fechData = fetch('https://api.nytimes.com/svc/topstories/v2/science.json?api-key=PL641sN6pmAS2hLTqKdJebGqzTW4LIYH')
-.then(responce => responce.json())
-.then(articles => {
-    console.log(articles.results);
-    const info = articles.results;
-    info.forEach(oneArticle => {
-        console.log(oneArticle.section);
-        console.log(oneArticle.title);
-        console.log(oneArticle.abstract);
-        console.log(oneArticle.short_url)
-        
+const fechData = async () => {
+    try {
+        const res = await fetch('https://api.nytimes.com/svc/topstories/v2/science.json?api-key=PL641sN6pmAS2hLTqKdJebGqzTW4LIYH');
+        const stories = await res.json();
+        const story = stories.results;
+        story.forEach(oneArticle => {
         const article = document.createElement('article');
         const link = document.createElement('button');
-
-        
         const photo = document.createElement('img');
-        console.log(oneArticle.multimedia[3].url)
-        photo.src = oneArticle.multimedia[3].url;
-        
         const section = document.createElement('div');
+        const abstract = document.createElement('p');
+        const title = document.createElement('h3');
+        const shortUrl = document.createElement('a');
+        const publishedDate = document.createElement('p');
+
+
+        photo.src = oneArticle.multimedia[0].url;
+        
+
         section.id = "section";
         section.innerText = `section: ${oneArticle.section}`;
         
-        const abstract = document.createElement('p');
+       
         abstract.id = "abstract";
         abstract.innerText = oneArticle.abstract;
         
-        const title = document.createElement('h3');
+        
         title.innerText = oneArticle.title;
         
-        const shortUrl = document.createElement('a');
+        
         shortUrl.href = oneArticle.short_url;
         shortUrl.innerText = "Read more";
         
-        const publishedDate = document.createElement('p');
-        publishedDate.innerText = `Published date: ${oneArticle.published_date}`;
+        
+        publishedDate.innerText = oneArticle.published_date.slice(0, 10);
         dataContainer.appendChild(article);
         article.append(section, photo, title, abstract, publishedDate,link);
         link.appendChild(shortUrl);
-    })
-})
+        })
+    } catch (error){
+        console.log(error);
+    }
+}
+
+fechData();
