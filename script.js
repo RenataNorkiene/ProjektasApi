85c5f40dfde45a1035f90c33447b12139150c141
RenataNorkiene <renata.norkiene@gmail.com>
2021-08-13 23:09:03 +0300

    edited html and js files

--- script.js
+++ script.js
@@ -6,38 +6,27 @@ const fechData = async () => {
         const stories = await res.json();
         const story = stories.results;
         story.forEach(oneArticle => {
-        const article = document.createElement('article');
-        const link = document.createElement('button');
-        const photo = document.createElement('img');
-        const section = document.createElement('div');
-        const abstract = document.createElement('p');
-        const title = document.createElement('h3');
-        const shortUrl = document.createElement('a');
-        const publishedDate = document.createElement('p');
+            const article = document.createElement('article');
+            const link = document.createElement('button');
+            const photo = document.createElement('img');
+            const section = document.createElement('div');
+            const abstract = document.createElement('p');
+            const title = document.createElement('h3');
+            const shortUrl = document.createElement('a');
+            const publishedDate = document.createElement('p');
 
-
-        photo.src = oneArticle.multimedia[0].url;
-        
-
-        section.id = "section";
-        section.innerText = `section: ${oneArticle.section}`;
-        
-       
-        abstract.id = "abstract";
-        abstract.innerText = oneArticle.abstract;
-        
-        
-        title.innerText = oneArticle.title;
-        
-        
-        shortUrl.href = oneArticle.short_url;
-        shortUrl.innerText = "Read more";
-        
-        
-        publishedDate.innerText = oneArticle.published_date.slice(0, 10);
-        dataContainer.appendChild(article);
-        article.append(section, photo, title, abstract, publishedDate,link);
-        link.appendChild(shortUrl);
+            photo.src = oneArticle.multimedia[0].url;
+            section.id = "section";
+            section.innerText = `section: ${oneArticle.section}`;
+            abstract.id = "abstract";
+            abstract.innerText = oneArticle.abstract;
+            title.innerText = oneArticle.title;
+            shortUrl.href = oneArticle.short_url;
+            shortUrl.innerText = "Read more";
+            publishedDate.innerText = oneArticle.published_date.slice(0, 10);
+            dataContainer.appendChild(article);
+            article.append(section, photo, title, abstract, publishedDate,link);
+            link.appendChild(shortUrl);
         })
     } catch (error){
         console.log(error);
